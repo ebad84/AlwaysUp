95a75a1ac88dc74095d4cb3ece4e55c0b84ea4f2
Mohammad Reza Ebadollah <63243209+ebad84@users.noreply.github.com>
2022-08-13 17:25:27 +0430

    Update app.py

--- app.py
+++ app.py
@@ -36,38 +36,32 @@ class AlwaysUpMainClass:
         else:
             self.database["userdata"][uname].update(data)
 
-    def refreshPA(self, uname ,passw) -> bool: #https://github.com/smartm13/AutoExtend_pythonanywhere/blob/master/pythonAnywhere_AutoRefresh.py
+    def refreshPA(self, uname ,passw) -> bool:
         print("[UNAME]", uname)
-        req = requests.Session()
-        response = req.get('https://www.pythonanywhere.com/login/', params={'next':'/user/{}/webapps/'.format(uname)}.items())
-        csrft=response.text[response.text.find("value='",response.text.find("csrfmiddlewaretoken"))+7:response.text.find("'",response.text.find("value='",response.text.find("csrfmiddlewaretoken"))+7)]
-        cookies = response.cookies.get_dict()
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Referer': 'https://www.pythonanywhere.com/login/?next=/user/{}/webapps/'.format(uname),
-        }
-        params={'next':'/user/{}/webapps/'.format(uname)}.items()
-        data = {
-          'csrfmiddlewaretoken': csrft,
-          'auth-username': '{}'.format(uname),
-          'auth-password': '{}'.format(passw),
-          'login_view-current_step': 'auth'
-        }
-        response2 = req.post('https://www.pythonanywhere.com/login/', headers=headers, params=params, cookies=cookies, data=data, allow_redirects = 1)
-        csrft=response2.text[response2.text.find("value='",response2.text.find("csrfmiddlewaretoken"))+7:response2.text.find("'",response2.text.find("value='",response2.text.find("csrfmiddlewaretoken"))+7)]
-        cookies=response2.cookies.get_dict()
-        #cookies['web_app_tab_type']='%23tab_id_{}_pythonanywhere_com'.format(uname)
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Referer': 'https://www.pythonanywhere.com/user/{}/webapps/'.format(uname),
-        }
-        data = {'csrfmiddlewaretoken': csrft}
-        #u = req.get("https://www.pythonanywhere.com/user/insprjprc/")
-        #print(u.is_redirect, u.url, u.content)
-        response3 = req.post('https://www.pythonanywhere.com/user/{0}/webapps/{0}.pythonanywhere.com/extend'.format(uname), headers=headers, cookies=cookies, data=data)
-        success=response3.request.url.count(uname)
-        #print(bool(success))
-        return bool(success)
+        username = uname
+        password = passw
+        try:
+            driver = webdriver.Firefox()
+            driver.get("https://pythonanywhere.com/login/")
+
+            username_select = driver.find_element(By.ID, 'id_auth-username')
+            username_select.send_keys(username)
+
+            password_select = driver.find_element(By.ID, 'id_auth-password')
+            password_select.send_keys(password)
+
+            login_select = driver.find_element(By.ID, 'id_next')
+            login_select.click()
+
+            web_apps_link = "https://www.pythonanywhere.com" + re.findall(r"<a id=\"id_web_app_link\" href=\"(.{1,})\">Web</a>", driver.page_source)[0]
+
+            driver.get(web_apps_link)
+
+            run_until_3month_select = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div/div/div[6]/div/div/div/form/input[2]')
+            run_until_3month_select.click()
+            return True
+        except:
+            return False
 
     def register(self, username, password):
         now = int(time.time())
